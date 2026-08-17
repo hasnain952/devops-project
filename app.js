eec5a60daae4ee45b03c0b13c999a8f285717ea9
hasnain952 <hasnain101syed@gmail.com>
2026-08-17 15:14:56 +0100

    Clean project and prepare for GitHub push

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello DevOps World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
